84d70a7cfefa4d8f2229984f56bdf6231c1cb3f7
kostyastrong <kostyakostya872@gmail.com>
2021-05-16 22:17:40 +0300

    small avl turn

--- AVL_tree.h
+++ AVL_tree.h
@@ -4,6 +4,7 @@
 #endif // AVL_TREE_H
 
 #include "common_parts_trees.h"
+#include <iostream>
 
 struct node_avl {
     node_avl* left, * right;
@@ -11,14 +12,47 @@ struct node_avl {
 };
 
 void SmallTurn(node_avl *& now) {
-    int bet = now->left->h - now->right->h;
+    node_avl * left = now->left, * right = now->right;
+
+    try{
+        if (left->left->h >= left->right->h) {
+            node_avl * c = left->right;
+            left->right = now;
+            now->left = c;
+            now = left;
+            return;
+        } else if (right->right->h >= right->left->h) {
+            node_avl * c = right->left;
+            right->left = now;
+            now->right = c;
+            now = right;
+        } else {
+            throw 42;
+        }
+    } catch (int i) {
+        std::cout << "NOT A SMALL TURN\n";
+        exit(0);
+    } catch (...) {
+        std::cout << "WTF HAPPENED IN SMALL TURN\N";
+        exit(0);
+    }
+}
+
+void BigTurn(node_avl *& now) {
+
+}
+void Turn(node_avl *& now) {  // h and h+2, not more
+    node_avl * left = now->left, * right = now->right;
+    int bet = left->h - right->h;
+
     if (bet < 0) bet = -bet;
     if (bet <= 1) {
         return;
+    }
 
-    } if (now->left->h > now->right->h) {
-        if (now->left->left->h >= now->left->right->h) {
-            node_avl
-        }
+    if (left->h > right->h) {
+        if (left->left->h >= left->right->h) {
+            SmallTurn(now);
+        } else {
     }
 }
